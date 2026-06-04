e73ba7de1db72323bb3da78f6f771f9f643af828
daniel <vega.enrique.martin@gmail.com>
2026-06-03 20:21:24 -0600

    init: proyecto inicial y estructura limpia en el caso de uso Task

--- src/tasks/application/create-task.use-case.ts
+++ src/tasks/application/create-task.use-case.ts
@@ -1,1 +1,18 @@
 // Capa de aplicacion ( caso de uso)
+
+import { Inject, Injectable } from "@nestjs/common";
+import type { ItaskRepository } from "../domain/task.repository.interface";
+import { ItaskRepositoryToken } from "../domain/task.repository.interface";
+
+@Injectable()
+export class CreateTaskUseCase {
+    constructor(
+        @Inject(ItaskRepositoryToken)
+        private readonly taskRepository: ItaskRepository,
+    ){}
+}
+
+//! git remote add origin git@github.com:ovee1/dov-giri6091-api.git
+
+
+//! git commit -m "init: proyecto inicial y estructura limpia en el caso de uso Task"
