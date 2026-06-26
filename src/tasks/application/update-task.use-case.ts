141358e0997f9d56e670b5e5b9493039831cadea
daniel <vega.enrique.martin@gmail.com>
2026-06-25 19:51:44 -0600

    fix: actualizacion de la implementación de tareas

--- src/tasks/application/update-task.use-case.ts
+++ src/tasks/application/update-task.use-case.ts
@@ -20,9 +20,6 @@ export class UpdateTaskUseCase {
        if (updateData.title != undefined) task.title = updateData.title;
        if (updateData.description != undefined) task.description = updateData.description;
        if (updateData.status != undefined){
-        if (updateData.status === 'COMPLETED')
-            task.complete();
-        else
             task.status = updateData.status;
        }
        return this.taskRepository.update(task);
--- src/tasks/infrastructure/persistence/task.repository.prisma.impl.ts
+++ src/tasks/infrastructure/persistence/task.repository.prisma.impl.ts
@@ -24,14 +24,33 @@ export class TaskRepositoryPrismaImpl implements ItaskRepository{
 
         return tasks;
     }
-    findById(id: number): Promise<Task | null> {
-        throw new Error("Method not implemented.");
+     async findById(id: number): Promise<Task | null> {
+        const task = await this.prisma.task.findUnique({
+            where: { id }
+
+        }) as Task | null;
+
+        return task;
     }
-    update(task: Task): Promise<Task> {
-        throw new Error("Method not implemented.");
+    async update(task: Task): Promise<Task> {
+        const update = await this.prisma.task.update({
+            where: { id: task.id },
+            data: {
+                title: task.title,
+                description: task.description,
+                status: task.status
+            }
+        }) as Task;
+        return update;
     }
-    delete(id: number): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    async delete(id: number): Promise<boolean> {
+        try {
+            await this.prisma.task.delete({ where: { id} });
+            return true;
+        } catch (error) {
+            return false;
+            
+        }
     }
     
 }
